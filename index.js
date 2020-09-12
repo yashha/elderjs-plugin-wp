60d40bf2a2a2010b19c8d9802668e596efcc0a3f
yas17 <8055914+yas17@user.noreply.gitee.com>
2020-09-12 02:42:10 +0200

    added basic plugin code

--- index.js
+++ index.js
@@ -1,58 +1,33 @@
 const plugin = {
-  name: 'elderjs-plugin-wp',
+  name: "elderjs-plugin-wp",
   description: `This plugin provides an easy wordpress integration for Elder.js using the WP-API npm package.`,
   init: (plugin) => {
-    // this is a sync function that runs on plugin initialization.
-    // if you need async, it is recommended that you extract the async logic to run on the 'bootstrap' hook.
-
-    // Plugins have their own closure scope. This means that if you set:
-    // plugin.init = true
-    // you will have access to plugin.init in all of your hooks.
-    // this data can be updated in hooks and will be persistent between page loads.
-    
-    // IMPORTANT: It is important to note that since builds are run across child processes, 
-    // the 'plugin' object is not consistent across all processes.
-
-    // Plugins also get the build settings (plugin.settings) and the config (plugin.config) settings. 
- 
+    const { config } = plugin;
+    if (!endpoint) {
+      console.log("elderjs-plugin-wp: No endpoint provided in config.");
+    }
+    let wp = new WPApi({
+      endpoint: config.endpoint,
+    });
+    plugin.wp = wp;
     return plugin;
   },
   hooks: [
     {
-      hook: 'bootstrap',
-      name: 'yourFirstHook',
-      description: `A description of what this hook does.`,
+      hook: "bootstrap",
+      name: "addWPToDataObject",
+      description: `Addes the intialized wordpress api object to the data object.`,
       priority: 50,
-      run: async ({ plugin, routes }) => {
-
-        // all props and mutations are detailed here: https://github.com/Elderjs/elderjs/blob/master/src/hookInterface/hookInterface.ts
-        // if you are looking for details on what a prop or mutation represents you can read this: https://github.com/Elderjs/elderjs/blob/master/src/hookInterface/hookEntityDefinitions.ts
-
-        // here is how you'd read the init property set in the init() function
-        if(plugin.init){
-          plugin.bootstrapRan = true;
-          return {
-            plugin,
-          };
-        }
-      }
-    },
-    {
-      hook: 'request',
-      name: 'yourSecondHook',
-      description: `A description of what this hook does.`,
-      priority: 50,
-      run: async ({ plugin, routes }) => {
-        if(plugin.init){
-          return {
-            plugin,
-          };
-        }
-      }
+      run: async ({ data, plugin }) => {
+        return {
+          data,
+          wp: plugin.wp,
+        };
+      },
     },
   ],
-  config: { // here is where you set the default configs for your plugin. These are merged with the configs found in the user's elder.config.js file.
-    doMagic: true,
+  config: {
+    endpoint: null,
   },
 };
 
